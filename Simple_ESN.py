9c01bae57387d60cd80d008015c6d88989e92078
sd3ntato <valeriomariani52@gmail.com>
2021-04-18 00:03:47 +0200

    ritrovato altro codice

--- Simple_ESN.py
+++ Simple_ESN.py
@@ -63,6 +63,22 @@ class ESN():
     wash_len = min(int(len(X)/3),500)
     return np.mean( np.square( y[wash_len:] - out[wash_len:] ) ) 
 
+################################# ESP INDEX #################################
+
+def ESP_Index(esn,data,P,T):
+  esn.x = np.zeros( ( esn.Nr,1 ) ) # setto stato iniziale a 0
+  # orbita x0. shape( len(data), (esn.N*esn.Nr), 1), ovvero un vettore di vettori colonna, ognuno dei quali rappresenta l'i-esimo stato (come concatenzione stati reservoir)
+  s0 = np.array( list( esn.compute_state(d).reshape(-1,1) for d in data ) )  
+  D = np.zeros( P ) # inizializzo vettore D
+  for i in range( P ):
+    esn.x = np.random.rand( esn.Nr, 1 ) # setto stato iniziale a vettore randomico
+    si = np.array( list( esn.compute_state(d).reshape(-1,1) for d in data ) ) # orbita xi.
+    d = np.zeros( np.size(data,axis=0) - T ) # per contenere distanze euclidee tra stati al t-esimo passo
+    for t in range( T, np.size(data,axis=0) ):
+      d[t-T] = np.linalg.norm( s0[t] - si[t] ) # d[t] contiene distanza tra stati al t-esimo passo, calcolati da stati iniziali diversi
+    D[i] = np.mean( d ) # media distanze stati per due stati iniziali diversi
+  return np.mean( D ) # a grandi linee media distanze stati per tanti stati iniziali diversi
+
 
 ################################# DIMENSIONE SPAZIO STATI #################################
 # calcola dimensione spazio stati 
